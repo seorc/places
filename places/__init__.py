9cc87f8eed2203fb7626dd2571e143a22ce1ebc6
Daniel Abraján <seorc.d@gmail.com>
2019-02-16 15:02:31 -0600

    Add the database schema

--- places/__init__.py
+++ places/__init__.py
@@ -1,11 +1,15 @@
 import os
 from flask import Flask
 from . import catalog
+from . import db
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(SECRET_KEY='topsecretmboy')
+    app.config.from_mapping(
+        SECRET_KEY='topsecretmboy',
+        DATABASE=os.path.join(app.instance_path, 'places.sqlite')
+    )
 
     if test_config is None:
         app.config.from_envvar('PLACES_CFG', silent=True)
@@ -15,7 +19,10 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # Register blueprints
+    # Register blueprints.
     app.register_blueprint(catalog.bp)
+
+    # Setup database access.
+    db.init_app(app)
 
     return app
---
+++ places/schema.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS places;
+
+CREATE VIRTUAL TABLE places
+USING FTS5(document);
