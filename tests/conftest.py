1dc6a8a3649fb921c1559d589b69c9bb54685711
Daniel Abraján <seorc.d@gmail.com>
2019-02-19 23:13:49 -0600

    Test the loader helper functions

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,6 +13,27 @@ def test_file_path():
 
 
 @pytest.fixture
+def sample_file_row():
+    return {
+        'd_codigo': '01000',
+        'd_asenta': 'San Ángel',
+        'd_tipo_asenta': 'Colonia',
+        'D_mnpio': 'Álvaro Obregón',
+        'd_estado': 'Ciudad de México',
+        'd_ciudad': 'Ciudad de México',
+        'd_CP': '01001',
+        'c_estado': '09',
+        'c_oficina': '01001',
+        'c_CP': '',
+        'c_tipo_asenta': '09',
+        'c_mnpio': '010',
+        'id_asenta_cpcons': '0001',
+        'd_zona': 'Urbano',
+        'c_cve_ciudad': '01',
+    }
+
+
+@pytest.fixture
 def app():
     db_file, db_path = tempfile.mkstemp()
 
--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,4 +1,7 @@
+import json
 from places.db import get_db
+from places.loader import join_row, to_json
+
 
 def test_load_file(app, runner, test_file_path):
     runner.invoke(args=['load-file', test_file_path])
@@ -8,3 +11,22 @@ def test_load_file(app, runner, test_file_path):
             'select count(*) from places'
         ).fetchone()[0] == 3
 
+
+def test_join_row(sample_file_row):
+    expected = '01000 San Ángel Colonia Álvaro Obregón' \
+        ' Ciudad de México Ciudad de México'
+    assert join_row(sample_file_row) == expected
+
+
+def test_to_json(sample_file_row):
+    expected = json.dumps({
+        'postal_code': sample_file_row['d_codigo'],
+        'neighborhood': sample_file_row['d_asenta'],
+        'neighborhood_type': sample_file_row['d_tipo_asenta'],
+        'municipality': sample_file_row['D_mnpio'],
+        'state': sample_file_row['d_estado'],
+        'city': sample_file_row['d_ciudad'],
+        'zone': sample_file_row['d_zona'],
+        'state_code': sample_file_row['c_estado'],
+    })
+    assert to_json(sample_file_row) == expected
